f8a4d23676d3c71c90046cdcc2da3f51f1cf7cad
Stanley Cheah <sccheah@ucdavis.edu>
2018-09-29 19:06:04 -0700

    removed unnecessary comments

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-// TODO: use routes, create post schema
-
 const posts = require('./routes/api/posts');
 const app = express();
 
@@ -19,26 +17,6 @@ mongoose
 
 app.use(bodyParser.json());
 app.use('/api/posts', posts);
-
-// app.get('/api/posts', (req, res) => {
-//   res.json(posts);
-// });
-
-// app.post('/api/posts', (req, res) => {
-//   console.log('received: ', req.body.newPost);
-//   posts.unshift(req.body.newPost);
-
-//   res.end('POST successful', req.body.newPost);
-// });
-
-// app.delete('/api/posts', (req, res) => {
-//   console.log('deleting...', req.body.id);
-
-//   let index = posts.findIndex(post => post.id === req.body.id);
-//   posts.splice(index, 1);
-
-//   res.end('DELETE successful: ', req.body.id);
-// });
 
 const port = 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
